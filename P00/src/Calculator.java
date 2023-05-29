6b999ea46172d54fbf3ff5d4c26db099d8dc5eff
22033840 <22033840@LAPTOP-FM76OMUC>
2023-05-29 15:16:49 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,19 @@
+
+public class Calculator {
+   public int add(int a, int b) {
+	   return a + b;
+   }
+   
+   public int subtract(int a, int b) {
+	   return a - b;
+   }
+   public int multi(int a, int b) {
+	   return a * b;
+   }
+   public int divide(int a, int b) {
+	   return a / b;
+   }
+   public double multi2(double a, double b) {
+	   return a * b;
+   }
+}
---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		int a = 10;
+		int b = 5;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.divide(a,b);
+		int expected = 2;
+		assertEquals (expected, actual);
+	}
+}
